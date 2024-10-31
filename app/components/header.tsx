c527a3a2667345a89434ef59325a800ba69b9e30
Dino0204 <s24020@gsm.hs.kr>
2024-10-31 20:00:36 +0900

    fix: header 수정

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -26,13 +26,13 @@ export default function Header() {
           <Image src={Arrow} alt="right" />
           <Image src={Repeat} alt="repeat" />
         </article>
-        <article className='flex flex-1 justify-center items-center'>
+        <article className='flex flex-1 justify-center items-center border p-2'>
           <Image src={AppleLogo} alt="appleLogo" />
         </article>
         <article className='flex flex-1 justify-center items-center'>
           <div className='flex justify-between w-[80%]'>
             <input type="range" min="0" max="100" defaultValue="0" />
-            <div className='flex'>
+            <div className='flex gap-4'>
               <Image src={PlayList} alt="playlist" />
               <button className='flex items-center text-[white] font-bold text-[13px] px-3 py-1 bg-red-500 rounded-md'>
                 <Image src={Profile} alt="profile" className='mr-[6px]' />
@@ -42,6 +42,6 @@ export default function Header() {
           </div>
         </article>
       </section>
-    </header>
+    </header >
   )
 }
